3bc81eff6afba5d3b71824379366ddde302175c5
miashore <mcbashore@gmail.com>
2017-03-13 15:13:12 -0700

    added firebase files

---
+++ firebaseConfig.js
@@ -0,0 +1,7 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBLUKQhm-jySiAw4SwL1rmi1aPnAxZgtTs",
+    authDomain: "c217tictactoe.firebaseapp.com",
+    databaseURL: "https://c217tictactoe.firebaseio.com",
+    storageBucket: "c217tictactoe.appspot.com",
+    messagingSenderId: "67024523893"
+};
