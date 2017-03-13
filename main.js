470b9b2133b642ac0952b47abfced48370429559
Sean Mee <Sean Mee>
2017-03-13 13:05:15 -0700

    OOP started

--- main.js
+++ main.js
@@ -1,37 +1,42 @@
-
- var counter = 0;
-$(document).ready(function(){
-
-    function changePlayer(){
-        $('.gameCells').click(function(){
-            if(counter%2 !== 0) {
-                $('#playerOne').removeClass('selected');
-                $('#playerTwo').addClass('selected');
-            }
-            else{
-                $('#playerTwo').removeClass('selected');
-                $('#playerOne').addClass('selected');
-            }
-        });
-
+var counter = 0;
+var players = [
+    {
+        symbol:'X',
+        name:'dude',
+        onBecomeCurrentPlayer: function(){
+            $('#playerOne').removeClass('selected');
+            $('#playerTwo').addClass('selected');
+        }
+    },
+    {
+        symbol: 'O',
+        name:'dudette',
+        onBecomeCurrentPlayer: function(){
+            $('#playerTwo').removeClass('selected');
+            $('#playerOne').addClass('selected');
+        }
     }
-
-    function cellClick(){
-        $('.gameCells').click(function(){
-            if (counter%2 === 0 && $(this).text()==='') {
-                $(this).text('X');
-            }
-            else if(counter%2 !== 0 && $(this).text() ===''){
-                $(this).text('O');
-            }
-            counter++;
-        });
-    }
-
-    function clickHandler(){
-        cellClick();
-        changePlayer();
-    }
-    clickHandler();
+];
+var currentPlayer = 0;
+$(document).ready(function() {
+    applyClickHandlers();
 
 });
+
+function applyClickHandlers(){
+    $('.gameCells').click(cellClickHandler);
+}
+function cellClickHandler(){
+    changePlayer();
+    cellShowSymbol(this);
+}
+function changePlayer(){
+        currentPlayer = 1-currentPlayer;
+        players[currentPlayer].onBecomeCurrentPlayer();
+
+}
+
+function cellShowSymbol(cellThatWasClicked){
+    $(cellThatWasClicked).text(players[currentPlayer].symbol)
+}
+
